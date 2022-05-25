fdc33ddcdb6c5181365387eea6af3fa37ab7c10a
Shazi <Shazi.saremi@torrens.edu.au>
2022-05-25 07:37:33 +0430

    Update

---
+++ Module 1/Samples/Hello_World_V1/main.cpp
@@ -0,0 +1,16 @@
+// Commenting Code:
+// Hello World
+// A First C++ Program
+
+// Including Other Files:
+#include <iostream>
+
+// Defining the main( ) Function:
+int main() {
+
+    // Displaying Text through the Standard Output:
+    std::cout << "Hello, World!" << std::endl;
+
+    // Returning a Value from main( ):
+    return(0);
+}
