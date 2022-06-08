e515a6e1240a851c5f804ecb53eac176a34d14af
Shazi <Shazi.saremi@torrens.edu.au>
2022-06-08 23:10:34 +0430

    Update

--- Module 1/Samples/6.Game_Stats_V3/main.cpp
+++ Module 1/Samples/6.Game_Stats_V3/main.cpp
@@ -1,20 +1,9 @@
-// Game Stats 3.0
-// Demonstrates constants
-//
 #include <iostream>
 using namespace std;
-
+enum colors{red=5, black};
+enum suit{heart, diamond=8, spade=3, club};
 int main() {
-    const int ALIEN_POINTS = 150;
-    int aliensKilled = 10;
-    int score = aliensKilled * ALIEN_POINTS;
-    cout << "score: " << score << endl;
-    enum difficulty {NOVICE, EASY, NORMAL, HARD, UNBEATABLE};
-    difficulty myDifficulty = EASY;
-
-    enum shipCost {FIGHTER_COST = 25, BOMBER_COST, CRUISER_COST = 50};
-    shipCost myShipCost = BOMBER_COST;
-    cout << "\nTo upgrade my ship to a Cruiser will cost " << (CRUISER_COST - myShipCost) << " Resource Points.\n";
-
+    cout <<"The value of enum color : "<<red<<","<<black;
+    cout <<"\nThe default value of enum suit : "<<heart<<","<<diamond<<","<<spade<<","<<club;
     return 0;
 }
