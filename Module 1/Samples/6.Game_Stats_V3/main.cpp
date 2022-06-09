880b841ebd8020d80e5d7a833d48e10720c720d2
Shahrzad Saremi <103140910+Shazisaremi@users.noreply.github.com>
2022-06-09 06:17:38 +0430

    Update main.cpp

--- Module 1/Samples/6.Game_Stats_V3/main.cpp
+++ Module 1/Samples/6.Game_Stats_V3/main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 using namespace std;
-enum colors{red=5, black};
-enum suit{heart, diamond=8, spade=3, club};
 int main() {
+    enum colors{red=5, black};
+    enum suit{heart, diamond=8, spade=3, club};
+    
     cout <<"The value of enum color : "<<red<<","<<black;
     cout <<"\nThe default value of enum suit : "<<heart<<","<<diamond<<","<<spade<<","<<club;
     return 0;
