67e66ad6af3068c24df3d12d285d019575f138eb
Shazi <Shazi.saremi@torrens.edu.au>
2022-07-13 20:09:02 +0430

    Module 5 Introduction, samples added.

---
+++ Module 5/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int fibonacci(int N);
+
+int main(){
+    int N;
+    cout <<"Enter Integer N : ";
+    cin >> N;
+    cout <<fibonacci(N)<<endl;
+}
+
+int fibonacci(int N){
+    //Place your code here
+    return ... ;
+}
